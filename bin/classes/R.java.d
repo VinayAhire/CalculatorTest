794a159cb27ff8147000acf87e128f219fcaa598
Vinay Ahire <vinaya@xpanxion.co.in>
2013-03-27 13:03:58 +0530

    after new build.

---
+++ bin/classes/R.java.d
@@ -0,0 +1,7 @@
+C:\Users\vinaya\21March\CalculatorTest\gen\com\calculator\test\R.java \
+ : C:\Users\vinaya\21March\CalculatorTest\res\drawable-hdpi\ic_launcher.png \
+C:\Users\vinaya\21March\CalculatorTest\res\drawable-ldpi\ic_launcher.png \
+C:\Users\vinaya\21March\CalculatorTest\res\drawable-mdpi\ic_launcher.png \
+C:\Users\vinaya\21March\CalculatorTest\res\drawable-xhdpi\ic_launcher.png \
+C:\Users\vinaya\21March\CalculatorTest\res\values\strings.xml \
+C:\Users\vinaya\21March\CalculatorTest\bin\AndroidManifest.xml \
