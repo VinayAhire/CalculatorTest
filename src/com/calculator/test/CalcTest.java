01e8fa69877b443d22efb6d800b944d1a23893d5
Vinay Ahire <vinaya@xpanxion.co.in>
2013-05-06 13:18:20 +0530

    vinay

--- src/com/calculator/test/CalcTest.java
+++ src/com/calculator/test/CalcTest.java
@@ -1,11 +1,16 @@
 package com.calculator.test;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.EditText;
 
 import com.calculator.Main;
 import com.calculator.R;
 import com.jayway.android.robotium.solo.Solo;
+
 
 public class CalcTest extends ActivityInstrumentationTestCase2<Main>{
 
@@ -15,13 +20,28 @@ public class CalcTest extends ActivityInstrumentationTestCase2<Main>{
 		super(Main.class);
 	}
 	
+	@Before
 	protected void setUp() throws Exception {
 		super.setUp();
 		solo = new Solo(getInstrumentation(), getActivity());
+		
+		/*LogConfigurator logConfigurator = new LogConfigurator(); 
+		logConfigurator.setFileName(Environment.getExternalStorageDirectory() 
+		+ File.separator + "MyApp" + File.separator + "logs" 
+		+ File.separator + "log4j.txt"); 
+		logConfigurator.setRootLevel(Level.DEBUG); 
+		logConfigurator.setLevel("org.apache", Level.ERROR); 
+		logConfigurator.setFilePattern("%d %-5p [%c{2}]-[%L] %m%n"); 
+		logConfigurator.setMaxFileSize(1024 * 1024 * 5); 
+		logConfigurator.setImmediateFlush(true); 
+		logConfigurator.configure(); 
+		Logger log = Logger.getLogger(Main.class); 
+		log.info("My Application Created");*/
 
 	}
 	
-	public void testA(){
+	@Test
+	public void A(){
 		EditText FirstValue = (EditText) solo.getView(R.id.EditText01);
 		solo.enterText(FirstValue, "5");
 		
@@ -33,6 +53,7 @@ public class CalcTest extends ActivityInstrumentationTestCase2<Main>{
 		//System.out.println("Hello Calculator");
 	}
 	
+	@After
 	protected void tearDown() throws Exception {
 		try {
 			solo.finalize();
@@ -42,9 +63,7 @@ public class CalcTest extends ActivityInstrumentationTestCase2<Main>{
 
 		}
 		getActivity().finish();
-
 		super.tearDown();
-
 	}
 	
 }
